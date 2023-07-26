1c5ac525cd0f54e7058976458a047cdba2ba7f20
dattatraySushir <sushirdattatray@gmail.com>
2023-07-26 11:29:21 +0530

    added program 1 class

---
+++ NewPractise/Polymorphism/Program1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewPractise.Polymorphism
+{
+    internal class Program1
+    {
+        public void M1()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
